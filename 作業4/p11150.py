b2f41056127bbdbea540278603d2d10ec2b4a9d6
ianbbbanian5476 <69791767+ianbbbanian5476@users.noreply.github.com>
2023-02-26 15:02:59 +0800

    Update p11150.py

--- 作業4/p11150.py
+++ 作業4/p11150.py
@@ -1,16 +1,28 @@
-while True:
-    n = input()
-    if n == '':
-        break
-    n = int(n)
-    count = n
-    if n % 3 != 0:
-        n = (n//3 + 1) * 3
-    count += n//3
-    n = n//3
-    while n >= 3:
-        if n % 3 != 0:
-            n = (n//3 + 1) * 3
-        count += n//3
-        n = n//3
-    print(count)
+#while True:
+#    n = input()
+#    if n == '':
+#        break
+#    n = int(n)
+#    count = n
+#    if n % 3 != 0:
+#        n = (n//3 + 1) * 3
+#    count += n//3
+#    n = n//3
+#    while n >= 3:
+#        if n % 3 != 0:
+#            n = (n//3 + 1) * 3
+#        count += n//3
+#        n = n//3
+#    print(count)
+
+
+def iszhengwei(s:str):
+    s = s.split(' ')
+    #if s == 'zhengwei':
+    #    return True
+    return s
+
+print(iszhengwei(5))
+
+
+
